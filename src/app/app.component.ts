5e857b392c7def26416e7852663556522abc6955
Hotchop <mirandanic1996@gmail.com>
2024-07-23 18:30:23 -0300

    Header and Routing Structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/shared/header/header.component";
+import { FooterComponent } from "./components/shared/footer/footer.component";
+import {RouterModule} from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent,RouterModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NgModule } from '@angular/core';
+import { AboutComponent } from './pages/about/about.component';
+import { ProyectsComponent } from './pages/proyects/proyects.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { SecretComponent } from './pages/secret/secret.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home',component:HomeComponent},
+    {path:'about',component:AboutComponent},
+    {path:'proyects',component:ProyectsComponent},
+    {path:'contact',component:ContactComponent},
+    {path:'secret',component:SecretComponent},
+    {path:'**',redirectTo:'home'},
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes,{
+      scrollPositionRestoration: 'enabled'
+    })],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
